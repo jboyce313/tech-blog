c30ac84411ec651cfa994082320abb70e831357b
Jacob Boyce <jboyce313@gmail.com>
2023-04-03 14:18:22 -0500

    set up models for user, post, and comment

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+module.exports = { User, Post, Comment };
