f468e3e6b5c8945485954ded3b55165a202e5f08
Jacob Boyce <jboyce313@gmail.com>
2023-04-03 15:34:15 -0500

    set up route infrastructure

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
 const path = require("path");
 const express = require("express");
-// Import express-session
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-
 const routes = require("./controllers");
+require("dotenv").config();
+
 const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
 // Set up sessions
 const sess = {
   secret: "Super secret secret",
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
   resave: false,
   saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
-
-const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
