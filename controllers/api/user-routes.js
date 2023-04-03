f30cdb263a6ce685a67ddc91fae73c95a692c47e
Jacob Boyce <jboyce313@gmail.com>
2023-04-03 15:59:11 -0500

    added route to log in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -17,4 +17,44 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+    if (!userData) {
+      res.statusMessage = "There is no account associated with that username.";
+      res.status(404).json({
+        message: "There is no account associated with that username.",
+      });
+      return;
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      userData.password
+    );
+
+    if (!validPassword) {
+      res.statusMessage = "Incorrect password.";
+      res.status(400).json({ message: "Incorrect password." });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      //   console.log(
+      //     "🚀 ~ file: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie",
+      //     req.session.cookie
+      //   );
+
+      res
+        .status(200)
+        .json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
