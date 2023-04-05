73b4e991402414100f1bab185189184c348053c7
Jacob Boyce <jboyce313@gmail.com>
2023-04-05 15:16:42 -0500

    added seeds for user and post

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,19 @@
 const sequelize = require("../config/connection");
 const { User, Post, Comment } = require("../models/");
 const postData = require("./post-seeds.json");
+const bcrypt = require("bcrypt");
+const userData = require("./user-seeds.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  for (const user of userData) {
+    const passwordHashed = await bcrypt.hash(user.password, 10);
+    const newUser = await User.create({
+      ...user,
+      password: passwordHashed,
+    });
+  }
 
   const posts = await Post.bulkCreate(postData, {
     individualHooks: true,
