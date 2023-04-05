137492be05235476460177664ee629648f459c93
Jacob Boyce <jboyce313@gmail.com>
2023-04-05 15:50:21 -0500

    added to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,18 @@ const router = require("express").Router();
 const { User, Post, Comment } = require("../models/");
 
 router.get("/", async (req, res) => {
-  res.render("homepage");
+  try {
+    // const postData = await Post.findAll({ include: [{ model: User }] });
+    const postData = await Post.findAll();
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      posts,
+      //   loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
