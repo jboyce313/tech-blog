259f6145aff79c29fec4a26bf8f614419898c52e
Jacob Boyce <jboyce313@gmail.com>
2023-04-07 10:16:20 -0500

    connected login.handlebars to backend so you can log in

---
+++ public/js/login.js
@@ -0,0 +1,20 @@
+document.querySelector(".login").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const username = document.querySelector(".username").value.trim();
+  const password = document.querySelector(".password").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to log in.");
+    }
+  }
+});
