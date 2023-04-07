b78e9a3d024390110d30a90296c91440a5c34a4f
Jacob Boyce <jboyce313@gmail.com>
2023-04-07 14:16:43 -0500

    added ability to log out

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render("homepage", {
       posts,
-      //   loggedIn: req.session.loggedIn,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     console.log(err);
